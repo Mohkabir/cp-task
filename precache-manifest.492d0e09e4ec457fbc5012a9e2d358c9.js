03deeeea23f748fa850a5cee08a12a2565c80868
mohammed kabir <kbshowkb@gmail.com>
2021-02-18 23:53:04 -1200

    Updates

---
+++ precache-manifest.492d0e09e4ec457fbc5012a9e2d358c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f0051eaf49a747f8e7fc47415f3937",
+    "url": "/cp-task/index.html"
+  },
+  {
+    "revision": "4f2ffd5c8cbc5e3fd3e9",
+    "url": "/cp-task/static/css/main.4796e6d7.chunk.css"
+  },
+  {
+    "revision": "69e44c5eb5af26546eb4",
+    "url": "/cp-task/static/js/2.9e66fa05.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/cp-task/static/js/2.9e66fa05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2ffd5c8cbc5e3fd3e9",
+    "url": "/cp-task/static/js/main.9ee956d3.chunk.js"
+  },
+  {
+    "revision": "534243736682cb666b4e",
+    "url": "/cp-task/static/js/runtime-main.0639665a.js"
+  },
+  {
+    "revision": "5e2d1d993ff10f962955cdcb50535f06",
+    "url": "/cp-task/static/media/Ellipse2.5e2d1d99.jpg"
+  },
+  {
+    "revision": "1dabe7b042e7cea812629d71faf3875f",
+    "url": "/cp-task/static/media/Ellipse3.1dabe7b0.jpg"
+  },
+  {
+    "revision": "d34c1876093eed4dd84395adc34d595c",
+    "url": "/cp-task/static/media/Ellipse4.d34c1876.jpg"
+  }
+]);
